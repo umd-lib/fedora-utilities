e13f88264d9552c9407e8ec888bb419efe7a3b48
Paul Hammer <phammer@umd.edu>
2014-04-21 14:26:50 -0400

    LIBDC-747: fedora-utilities cleanup: reconcile FOXML
      Integrated UMfactory in order to add type to creation and to
      use DOutils functions for XPATH and Document printing
    
    https://issues.umd.edu/browse/LIBDC-747

--- src/main/java/edu/umd/lib/fedora/util/foxml/UMfactory.java
+++ src/main/java/edu/umd/lib/fedora/util/foxml/UMfactory.java
@@ -168,7 +168,7 @@ public class UMfactory {
    * 
    * @return
    */
-  public DOxml getDO(String pid) {
+  public DOxml getDO(String pid, String type) {
     
     // Get the DC form the pid source
     if( pid != null ) {
@@ -179,18 +179,26 @@ public class UMfactory {
     try {
       String host = sHost;
       String strURL = "http://"+ host + 
-        "/fedora/get/"+ pid + 
-        "/umd-bdef:amInfo/getAMInfo/";
+        "/fedora/get/"+ pid;
+      if( type.equals("UMAM")) {
+        strURL += "/umd-bdef:amInfo/getAMInfo/";
+      } else {
+        strURL += "/umd-bdef:doInfo/getDOInfo/";
+      }
       SAXReader reader = new SAXReader();
       Document thisDoc = reader.read(strURL);
       // Element root = thisDoc.getRootElement();
-      oDO = new DOxml();
+      if( thisDoc == null ) {
+        oDO = new DOxml();
+      } else {
+        oDO = new DOxml(thisDoc);
+      }
       
       return oDO;
       
     } catch (Exception e) {
       // TODO: handle exception
-      e.printStackTrace(System.out);
+      //e.printStackTrace(System.out);
       return null;
     }
     }
@@ -306,57 +314,4 @@ public class UMfactory {
 		return theseMethods;
 	} 
 	
-	/************************************************************* getXPath */
-	/**
-	 * Get a compiled XPath object for the expression.  Cache.
-	 */
-
-	@SuppressWarnings("unchecked")
-	public XPath getXPath(String strXPath) throws InvalidXPathException {
-
-		XPath xpath = null;
-
-		if (mXPath.containsKey(strXPath)) {
-			xpath = (XPath) mXPath.get(strXPath);
-
-		} else {
-			xpath = df.createXPath(strXPath);
-			xpath.setNamespaceURIs(namespace);
-			mXPath.put(strXPath, xpath);
-		}
-
-		return xpath;
-	}
-	
-	/**************************************************************** PrintDoc */
-	/**
-	 * Prints a document based on the class's printing directive string
-	 */
-	@SuppressWarnings("unused")
-	private boolean printDoc(Document docThisDocument, String strFile) {
-
-		strFile = strFile.replaceAll("umd:", "umd_");
-
-		try {
-
-			// Create the string of the filename to be written to
-			String strFileName = strFile;
-
-			// Set up the interminable number of structures for printing this
-			// puppy
-			OutputStreamWriter outWriter = new OutputStreamWriter(
-					new FileOutputStream(strFileName), "UTF-8");
-			OutputFormat format = OutputFormat.createPrettyPrint();
-			XMLWriter writer = new XMLWriter(outWriter, format);
-			writer.write(docThisDocument);
-			writer.close();
-
-		}
-
-		catch (Exception e) {
-			e.printStackTrace();
-		}
-
-		return true;
-	}
 }
